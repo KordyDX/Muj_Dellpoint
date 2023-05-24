6f6e67d56607a4c51be936594dc71e448a333a5d
Kristián Stefurak <majk123yt@gmail.com>
2023-05-24 13:41:06 +0200

    Update

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var searchInput = document.querySelector('.search');
-    var searchStylish = document.querySelector('.search_stylish');
-  
-    searchInput.addEventListener('focus', function() {
-      searchStylish.classList.add('focused');
-    });
-  
-    searchInput.addEventListener('blur', function() {
-      searchStylish.classList.remove('focused');
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var searchInput = document.querySelector('.search');
+  var searchStylish = document.querySelector('.search_stylish');
+  var navButton = document.querySelector('.nav_button');
+  var navContainer = document.querySelector('.nav_container');
+  var buttonClicked
+  searchInput.addEventListener('focus', function () {
+    searchStylish.classList.add('focused');
   });
+
+  searchInput.addEventListener('blur', function () {
+    searchStylish.classList.remove('focused');
+  });
+
+  navButton.addEventListener('click', function () {
+    navContainer.classList.toggle('focused');
+    navButton.classList.toggle('focused');
+  });
+});
